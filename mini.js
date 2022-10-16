8565405b753b930964b8683d96537bcbbb846487
Mohinur <hikmatovamohinur2007@gmail.com>
2022-10-16 22:17:22 +0500

    hello

---
+++ mini.js
@@ -0,0 +1,26 @@
+// let answers= []
+// let questions = [
+//     'Имя',
+//     'Фамилия',
+//     'Год рождения',
+//     'Пол'
+// ]
+// for (let i=0, i<questions.length; i++){
+//     answers [i] =prompt(questions[i])
+// }
+// console.log (answers)
+
+
+
+
+
+
+
+let age = prompt ('Время учения')
+    if (age<18){
+        alert('Последние годы')
+    }else if (age>7){
+        alert('Время учится')
+    }else{
+        alert ('Уже поздно')
+    }
